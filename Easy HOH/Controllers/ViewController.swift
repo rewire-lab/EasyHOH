0aa0ca03e6e414d396d248f2d8bf5d580a304b80
Mike <mjmurray4@gmail.com>
2020-04-14 16:07:49 -0700

    Complete first version.

--- Easy HOH/Controllers/ViewController.swift
+++ Easy HOH/Controllers/ViewController.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
 
-    var itemArray = ["Water", "Fuelings", "Lean"]
-    var count = 0
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadItems()
     }
 
 
@@ -29,18 +34,33 @@ class ViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "HabitItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
-        cell.detailTextLabel?.text = String(count)
+//        cell.textLabel?.text = itemArray[indexPath.row]
+//        cell.detailTextLabel?.text = String(count)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = String(item.count)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
-        print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].count += 1
+        
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+    }
+    
+    
+    @IBAction func resetCounts(_ sender: Any) {
+        print("got reset")
+        for item in itemArray {
+            item.count = 0
+        }
+        saveItems()
     }
     
     //MARK: - Add New Items
@@ -53,8 +73,12 @@ class ViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen when user clicks Add Item
-            self.itemArray.append(textField.text!)
-            self.tableView.reloadData()
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.count = 0
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -67,6 +91,28 @@ class ViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
         
     }
+ 
+    func saveItems() {
+        do {
+            try context.save()
+            
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    func loadItems() {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetchong data from context \(error)")
+        }
+    }
+    
     
 }
 
